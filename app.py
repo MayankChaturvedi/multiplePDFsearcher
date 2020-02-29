7fa4ec2fabf34a653e0550259c09e8e9a8cbaf23
Mayank Chaturvedi <chaturvedim4@gmail.com>
2020-02-29 22:30:26 +0530

    corrected the last file opening bug and added scrollbars

--- app.py
+++ app.py
@@ -1,8 +1,83 @@
 from tkinter import *
 import PyPDF2
 import re
-import os
 import webbrowser
+import os, sys, subprocess
+
+class DoubleScrolledFrame:
+    """
+    A vertically scrolled Frame that can be treated like any other Frame
+    ie it needs a master and layout and it can be a master.
+    keyword arguments are passed to the underlying Frame
+    except the keyword arguments 'width' and 'height', which
+    are passed to the underlying Canvas
+    note that a widget layed out in this frame will have Canvas as self.master,
+    if you subclass this there is no built in way for the children to access it.
+    You need to provide the controller separately.
+    """
+    def __init__(self, master, **kwargs):
+        width = kwargs.pop('width', None)
+        height = kwargs.pop('height', None)
+        self.outer = Frame(master, **kwargs)
+
+        self.vsb = Scrollbar(self.outer, orient=VERTICAL)
+        self.vsb.grid(row=0, column=1, sticky='ns')
+        self.hsb = Scrollbar(self.outer, orient=HORIZONTAL)
+        self.hsb.grid(row=1, column=0, sticky='ew')
+        self.canvas = Canvas(self.outer, highlightthickness=0, width=width, height=height)
+        self.canvas.grid(row=0, column=0, sticky='nsew')
+        self.outer.rowconfigure(0, weight=1)
+        self.outer.columnconfigure(0, weight=1)
+        self.canvas['yscrollcommand'] = self.vsb.set
+        self.canvas['xscrollcommand'] = self.hsb.set
+        # mouse scroll does not seem to work with just "bind"; You have
+        # to use "bind_all". Therefore to use multiple windows you have
+        # to bind_all in the current widget
+        self.canvas.bind("<Enter>", self._bind_mouse)
+        self.canvas.bind("<Leave>", self._unbind_mouse)
+        self.vsb['command'] = self.canvas.yview
+        self.hsb['command'] = self.canvas.xview
+
+        self.inner = Frame(self.canvas)
+        # pack the inner Frame into the Canvas with the topleft corner 4 pixels offset
+        self.canvas.create_window(4, 4, window=self.inner, anchor='nw')
+        self.inner.bind("<Configure>", self._on_frame_configure)
+
+        self.outer_attr = set(dir(Widget))
+
+    def __getattr__(self, item):
+        if item in self.outer_attr:
+            # geometry attributes etc (eg pack, destroy, tkraise) are passed on to self.outer
+            return getattr(self.outer, item)
+        else:
+            # all other attributes (_w, children, etc) are passed to self.inner
+            return getattr(self.inner, item)
+
+    def _on_frame_configure(self, event=None):
+        x1, y1, x2, y2 = self.canvas.bbox("all")
+        height = self.canvas.winfo_height()
+        width = self.canvas.winfo_width()
+        self.canvas.config(scrollregion = (0,0, max(x2, width), max(y2, height)))
+
+    def _bind_mouse(self, event=None):
+        self.canvas.bind_all("<4>", self._on_mousewheel)
+        self.canvas.bind_all("<5>", self._on_mousewheel)
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+    def _unbind_mouse(self, event=None):
+        self.canvas.unbind_all("<4>")
+        self.canvas.unbind_all("<5>")
+        self.canvas.unbind_all("<MouseWheel>")
+        
+    def _on_mousewheel(self, event):
+        """Linux uses event.num; Windows / Mac uses event.delta"""
+        func = self.canvas.xview_scroll if event.state & 1 else self.canvas.yview_scroll
+        if event.num == 4 or event.delta > 0:
+            func(-1, "units" )
+        elif event.num == 5 or event.delta < 0:
+            func(1, "units" )
+
+
 
 def callback(url):
     webbrowser.open_new(url)
@@ -37,7 +112,7 @@ def goforit(a,String):
     
 
 root = Tk()
-root.geometry("1000x900")
+root.geometry("900x800")
 
 root.title("Made with ♥️ for math peeps")
 
@@ -68,9 +143,11 @@ txt.pack()
 lbl2=Label(up,text="")
 lbl2.pack(side="right")
 
-down = Frame(root, borderwidth=0, relief="solid")
+dfrm=DoubleScrolledFrame(root,height=20, width=1000, borderwidth=2, relief=SUNKEN, background="light gray")
+dfrm.pack(side="bottom", expand=True)
 
-down.pack(side="bottom",expand=True,fill="both")
+
+
 
 scrollbar = Scrollbar(root)
 scrollbar.pack(side = RIGHT, fill = Y)
@@ -81,30 +158,37 @@ area.pack(expand=True, fill='both')
 
 scrollbar.config(command = area.yview)
 
-
+def open(filename):
+    if sys.platform == "win32":
+        os.startfile(filename)
+    else:
+        opener ="open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, filename])
+        
+        
 def fun(tString):
     area.delete('1.0', END)
-    for widget in down.winfo_children():
-        widget.destroy()
+    #for widget in down.winfo_children():
+    #widget.destroy()
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     files = filter(lambda f: f.endswith(('.pdf','.PDF')), files)
     tString
     global tot
     tot=0
+    tt=1
+    opfiles=[]
     for a in files:
         lst=goforit(a,tString)
-        tct=0
         for b in lst:
-            tct=1
             area.insert(END,b+'\n')
-        if tct==1:
-            link1 = Label(down, text=str(a), fg="blue", cursor="hand2")
-            link1.pack(side="left")
-            link1.bind("<Button-1>", lambda e: webbrowser.open_new(r"file://"+os.path.realpath(str(a))))
+            opfiles.append(str(a))
     if tot==0:
         area.insert(END," :( Ah shit! Here we go again. The word not found")
-        
-
+    else:
+        for i in range(0,len(opfiles)):
+            button = Button(dfrm,text=opfiles[i], command=lambda i=i: open(opfiles[i]))
+            button.pack(side="left")
+            
 def solveqry(a):
     fun(a)
     lbl2.configure(text="search sucessful")
